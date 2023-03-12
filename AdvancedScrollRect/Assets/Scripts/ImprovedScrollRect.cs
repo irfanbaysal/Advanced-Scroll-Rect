9ccd14598f4cf6bef64d1722f2a0e72aebd0705e
İrfan Yiğit Baysal <irfanyigitbaysal@rubygamestudio.com>
2023-03-12 14:15:42 +0300

    Improved Scroll Rect added

---
+++ AdvancedScrollRect/Assets/Scripts/ImprovedScrollRect.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace General.UI
+{
+    public class ImprovedScrollRect : ScrollRect
+    {
+        protected virtual bool IsHandheld => SystemInfo.deviceType == DeviceType.Handheld;
+        protected virtual bool IsDesktop => SystemInfo.deviceType == DeviceType.Desktop;
+
+        private readonly Dictionary<int, Vector2> _activeTouches = new Dictionary<int, Vector2>();
+        private const int MultiTouchCountTrigger = 1;
+        private Vector2 _scrollingOffset = Vector2.zero;
+        private bool _isScrolling;
+
+        public override void OnScroll(PointerEventData data)
+        {
+            base.OnScroll(data);
+            _isScrolling = data.IsScrolling();
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            UpdateDragCondition(false,Vector2.zero);
+        }
+
+        public override void OnBeginDrag(PointerEventData eventData)
+        {
+            switch (IsHandheld)
+            {
+                case true:
+                    {
+                        SetPointerID(eventData);
+                        if (Input.touchCount >= MultiTouchCountTrigger && !_isScrolling)
+                        {
+                            base.OnBeginDrag(eventData);
+                            UpdateDragCondition(true,eventData.position);
+                        }
+
+                        break;
+                    }
+                default:
+                    {
+                        if (IsDesktop)
+                        {
+                            base.OnBeginDrag(eventData);
+                        }
+
+                        break;
+                    }
+            }
+        }
+
+
+        public override void OnDrag(PointerEventData eventData)
+        {
+            switch (IsHandheld)
+            {
+                case true when Input.touchCount < MultiTouchCountTrigger || !_activeTouches.ContainsKey(eventData.pointerId):
+                    return;
+                case true:
+                    _scrollingOffset += eventData.position - _activeTouches[eventData.pointerId];
+                    SetPointerID(eventData);
+                    eventData.position = _scrollingOffset;
+                    base.OnDrag(eventData);
+                    break;
+                default:
+                    {
+                        if (IsDesktop)
+                        {
+                            base.OnDrag(eventData);
+                        }
+
+                        break;
+                    }
+            }
+        }
+        public override void OnEndDrag(PointerEventData eventData)
+        {
+            switch (IsHandheld)
+            {
+                case true:
+                {
+                    
+                        if (_activeTouches.ContainsKey(eventData.pointerId))
+                        {
+                            _activeTouches.Remove(eventData.pointerId);
+                        }
+
+                        if (Input.touchCount != MultiTouchCountTrigger)
+                        {
+                            return;
+                        }
+
+                        base.OnEndDrag(eventData);
+                        UpdateDragCondition(false,Vector2.zero);
+                        break;
+                    }
+                default:
+                    {
+                        if (IsDesktop)
+                        {
+                            base.OnEndDrag(eventData);
+                        }
+
+                        break;
+                    }
+            }
+        }
+
+        private void UpdateDragCondition(bool isBegan,Vector2 updatedOffset)
+        {
+            _isScrolling = isBegan;
+            _scrollingOffset = updatedOffset;
+        }
+
+        private void SetPointerID(PointerEventData pointerEventData)
+        {
+            _activeTouches[pointerEventData.pointerId] = pointerEventData.position;
+        }
+    }
+}
+
